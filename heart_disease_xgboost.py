ce74e86e648cd2fc4814e904df858602b23cbc4a
Kaustubh Gupta <160870736+Kaustubhgupta28@users.noreply.github.com>
2026-03-03 19:29:33 +0530

    Add StratifiedKFold and cross_val_score imports

--- heart_disease_xgboost.py
+++ heart_disease_xgboost.py
@@ -7,7 +7,7 @@ import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
 import seaborn as sns
-from sklearn.model_selection import train_test_split
+from sklearn.model_selection import train_test_split, StratifiedKFold, cross_val_score
 from sklearn.preprocessing import StandardScaler
 from sklearn.metrics import accuracy_score, roc_auc_score, confusion_matrix, classification_report, ConfusionMatrixDisplay, roc_curve, auc
 from xgboost import XGBClassifier
